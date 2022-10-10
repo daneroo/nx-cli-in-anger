3d4913086ae1b982bede1c05342588a8e175bcee
Daniel Lauzon <daniel.lauzon@gmail.com>
2022-10-10 17:56:49 -0400

    Fix linting for .eslintrc.cjs itself

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,31 @@
 module.exports = {
-    env: {
-        es2021: true,
-        node: true
-    },
-    extends: [
-        'eslint:recommended', 
-        'standard-with-typescript',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:@typescript-eslint/strict',
-        'prettier'
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: [
+      './tsconfig.json',
+      // Tell eslint how to lint this file
+      // with an extra config (./tsconfig.eslint.json)
+      // which includes this file specifically
+      // see https://stackoverflow.com/questions/61956555/why-is-typescript-eslint-parser-including-files-outside-of-those-configured-in
+      './tsconfig.eslint.json',
     ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json']
-    },
-    ignorePatterns: [
-        "node_modules/",
-        "dist/"
-    ],
-    rules: {
-        '@typescript-eslint/no-explicit-any': 'error'
-    },
+  },
+  ignorePatterns: ['node_modules/', 'dist/'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
 };
